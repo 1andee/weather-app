668e06595a5de9983cabdb4d7972a874f5e292f7
Ian Duke <ian@thefarmventures.com>
2017-10-22 21:48:47 -0400

    Rendering API results in browser as JSON

--- assets/main.js
+++ assets/main.js
@@ -2,13 +2,16 @@ $( document ).ready(function() {
     console.log( "document ready!" );
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position.coords.latitude, position.coords.longitude);
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        let fccWxApi = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
 
-        // TODO
-        // pass coordinates to API
-        // make API call
-        // get JSON back
-        // render JSON results in browser
+        $.getJSON(fccWxApi, {
+            format: "json"
+          })
+            .done((data) => {
+            $("#results").html(JSON.stringify(data));
+          });
       });
     } else {
       console.log('geolocation is *NOT* available')
