9f20f977a213c5b72783cac28188be3c696761c3
Ian Duke <ian@thefarmventures.com>
2017-10-25 22:02:01 -0400

    Show relevant icons depending on conditions

--- assets/main.js
+++ assets/main.js
@@ -15,13 +15,23 @@ $(document).ready(() => {
             let roundedTemp_min = Math.round(temp_min);
             let roundedTemp_max = Math.round(temp_max);
 
+            $("#currentHeader").html('<h3>Current Conditions</h3>');
+            $("#spinner").html('');
+
             $("#location").html(`${data.name}, ${data.sys['country']}`);
-            $("#conditions").html(data.weather[0].description);
+
+            data.weather.forEach((item) => {
+              generateIcon(item.main);
+              $("#conditions").append(`<p>${item.description}</p>`);
+            })
+
             $("#temp").html(`<h3>Temperature</h3><p>`+roundedTemp+`&deg;C</p>`);
             $("#px").html(`<h3>Pressure</h3><p>`+pressure+` millibars</p>`);
             $("#humidity").html(`<h3>Humidity</h3><p>`+humidity+`%</p>`);
             $("#temp_min").html(`<h3>Min Temp</h3><p>`+roundedTemp_min+`&deg;C</p>`);
             $("#temp_max").html(`<h3>Max Temp</h3><p>`+roundedTemp_max+`&deg;C</p>`);
+
+            $("#toggleContainer").addClass('show');
 
             $("#convertF").click(() => {
               convertFahrenheit(temp, temp_min, temp_max);
@@ -42,7 +52,6 @@ $(document).ready(() => {
 });
 
 convertFahrenheit = (temp, temp_min, temp_max) => {
-  console.log('converting to Fahrenheit');
   let convertedTemp = ((temp * 9) / 5) + 32;
   let convertedMin = ((temp_min * 9) / 5) + 32;
   let convertedMax = ((temp_max * 9) / 5) + 32;
@@ -55,4 +64,30 @@ convertCelsius = (roundedTemp, roundedTemp_min, roundedTemp_max) => {
   $("#temp").html(`<h3>Temperature</h3><p>`+roundedTemp+`&deg;C</p>`);
   $("#temp_min").html(`<h3>Min Temp</h3><p>`+roundedTemp_min+`&deg;C</p>`);
   $("#temp_max").html(`<h3>Max Temp</h3><p>`+roundedTemp_max+`&deg;C</p>`);
+}
+
+generateIcon = (condition) => {
+  switch(condition) {
+    case 'Thunderstorm':
+      $("#conditions").append(`<i class="fa fa-bolt fa-5x" aria-hidden="true"></i>`);
+      break;
+    case 'Drizzle':
+      $("#conditions").append(`<i class="fa fa-umbrella fa-5x" aria-hidden="true"></i>`);
+      break;
+    case 'Rain':
+      $("#conditions").append(`<i class="fa fa-umbrella fa-5x" aria-hidden="true"></i>`);
+      break;
+    case 'Snow':
+      $("#conditions").append(`<i class="fa fa-snowflake-o fa-5x" aria-hidden="true"></i>`);
+      break;
+    case 'Clouds':
+      $("#conditions").append(`<i class="fa fa-cloud fa-5x" aria-hidden="true"></i>`);
+      break;
+    case 'Clear':
+      $("#conditions").append(`<i class="fa fa-sun-o fa-5x" aria-hidden="true"></i>`);
+      break;
+    default:
+      $("#conditions").append(`<i class="fa fa-sun-o fa-5x" aria-hidden="true"></i>`);
+      break;
+    }
 }
