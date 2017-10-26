253a433890cbbaff2dcd9ddb9d8796016ec8d2d0
Ian Duke <ian@thefarmventures.com>
2017-10-25 21:26:58 -0400

    Conversion between C and F is working

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,4 @@
-$( document ).ready(function() {
-    console.log( "document ready!" );
+$(document).ready(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         let lat = position.coords.latitude;
@@ -11,16 +10,26 @@ $( document ).ready(function() {
           })
             .done((data) => {
             let {temp, pressure, humidity, temp_min, temp_max} = data.main;
+            let roundedTemp = Math.round(temp);
+            let roundedTemp_min = Math.round(temp_min);
+            let roundedTemp_max = Math.round(temp_max);
 
             $("#location").html(`${data.name}, ${data.sys['country']}`);
             $("#conditions").html(data.weather[0].description);
-            $("#temp").html(temp);
-            $("#px").html(pressure);
-            $("#humidity").html(humidity);
-            $("#temp_min").html(temp_min);
-            $("#temp_max").html(temp_max);
+            $("#temp").html(`<h3>Temperature</h3><p>`+roundedTemp+`&deg;C</p>`);
+            $("#px").html(`<h3>Pressure</h3><p>`+pressure+` millibars</p>`);
+            $("#humidity").html(`<h3>Humidity</h3><p>`+humidity+`%</p>`);
+            $("#temp_min").html(`<h3>Min Temp</h3><p>`+roundedTemp_min+`&deg;C</p>`);
+            $("#temp_max").html(`<h3>Max Temp</h3><p>`+roundedTemp_max+`&deg;C</p>`);
 
-            $("#results").html(JSON.stringify(data));
+            $("#convertF").click(() => {
+              convertFahrenheit(temp, temp_min, temp_max);
+            });
+
+            $("#convertC").click(() => {
+              convertCelsius(roundedTemp, roundedTemp_min, roundedTemp_max);
+            });
+
           });
       });
     } else {
@@ -30,3 +39,19 @@ $( document ).ready(function() {
       // Make API call
     }
 });
+
+convertFahrenheit = (temp, temp_min, temp_max) => {
+  console.log('converting to Fahrenheit');
+  let convertedTemp = ((temp * 9) / 5) + 32;
+  let convertedMin = ((temp_min * 9) / 5) + 32;
+  let convertedMax = ((temp_max * 9) / 5) + 32;
+  $("#temp").html(`<h3>Temperature</h3><p>`+Math.round(convertedTemp)+`&deg;F</p>`);
+  $("#temp_min").html(`<h3>Min Temp</h3><p>`+Math.round(convertedMin)+`&deg;F</p>`);
+  $("#temp_max").html(`<h3>Max Temp</h3><p>`+Math.round(convertedMax)+`&deg;F</p>`);
+}
+
+convertCelsius = (roundedTemp, roundedTemp_min, roundedTemp_max) => {
+  $("#temp").html(`<h3>Temperature</h3><p>`+roundedTemp+`&deg;C</p>`);
+  $("#temp_min").html(`<h3>Min Temp</h3><p>`+roundedTemp_min+`&deg;C</p>`);
+  $("#temp_max").html(`<h3>Max Temp</h3><p>`+roundedTemp_max+`&deg;C</p>`);
+}
